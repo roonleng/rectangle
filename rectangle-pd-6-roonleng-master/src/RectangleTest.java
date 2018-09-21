a3173650cef3a89a4001d59172a3d49b836ae402
roonleng <42977073+roonleng@users.noreply.github.com>
2018-09-20 20:12:57 -0400

    Add files via upload

---
+++ rectangle-pd-6-roonleng-master/src/RectangleTest.java
@@ -0,0 +1,24 @@
+import java.awt.Rectangle;
+
+public class RectangleTest {
+
+	public static void main(String[] args)
+	{
+		//look in Rectangle API under constructors to find what Rectangle(5,10,20,30) does
+		Rectangle box = new Rectangle(5, 10, 20, 30);
+		
+		
+		//TODO Move the rectangle (use a method from the API to move the rectangle
+		//                          by changing both the x and y coordinates)
+		
+		//TODO Print the following information in the following format
+		/*
+		 * Expected new x value: ... (print what you expect the x value to be)
+		 * Actual x value: ...  (use a method from the API to print the actual x value)
+		 * Expected new y value: ... (print what you expect the new y value to be)
+		 * Actual y value: ...  (use a method from the API to print the actual y value)
+		 */
+
+	}
+
+}
